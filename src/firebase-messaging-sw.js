c01e5f877d96eb5b4184724cae885f7e32c1b1c6
Abdoul Dosso <abdouldosso78@gmail.com>
2025-11-03 22:17:28 +0000

    implementation of fcm

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -18,7 +18,7 @@ messaging.onBackgroundMessage(function(payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/assets/icons/icon-192.png'
+    icon: '/assets/images/logo.png'
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
